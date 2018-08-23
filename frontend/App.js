b9525e571434a83bab4df99ac4537224feb99fb9
Alexander Mostovenko <alexmost1989@gmail.com>
2018-08-23 16:39:38 +0300

    implemented prerendering

--- frontend/App.js
+++ frontend/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, StaticRouter } from 'react-router-dom';
 import SpeakersList from './SpeakersList';
 import Speaker from './Speaker';
 import { Store } from './Store';
 
+const isNode = typeof window === 'undefined';
+
+const Router = isNode ? StaticRouter : BrowserRouter;
+
 const App = (props) => (
-  <Store>
-    <BrowserRouter>
+  <Store posts={props.posts}>
+    <Router location={props.location}>
       <Switch>
         <Route path="/" exact component={SpeakersList}/>
         <Route path="/speaker/:id" exact component={Speaker}/>
       </Switch>
-    </BrowserRouter>
+    </Router>
   </Store>
 );
 
--- frontend/index.js
+++ frontend/index.js
@@ -2,4 +2,6 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './App.js';
 
-ReactDOM.render(<App/>, document.getElementById('container'));
+const posts = JSON.parse(document.getElementById('posts').text);
+
+ReactDOM.hydrate(<App posts={posts} />, document.getElementById('container'));
