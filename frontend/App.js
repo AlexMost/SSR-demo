3e7b1d7a5fc178c920db9a5ebb7ed4d5f8043ec0
Alexander Mostovenko <alexmost1989@gmail.com>
2018-08-23 12:53:46 +0300

    put Store inside App

--- frontend/App.js
+++ frontend/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import SpeakersList from './SpeakersList';
 import Speaker from './Speaker';
+import { Store } from './Store';
 
 const App = (props) => (
-  <BrowserRouter>
-    <Switch>
-      <Route path="/" exact component={SpeakersList}/>
-      <Route path="/speaker/:id" exact component={Speaker}/>
-    </Switch>
-  </BrowserRouter>
+  <Store>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={SpeakersList}/>
+        <Route path="/speaker/:id" exact component={Speaker}/>
+      </Switch>
+    </BrowserRouter>
+  </Store>
 );
 
 export default App;
--- frontend/index.js
+++ frontend/index.js
@@ -1,21 +1,5 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './App.js';
-import { Store } from './Store';
 
-function tryReadPosts() {
-  const state = document.getElementById('state');
-  if (!state) return;
-  try {
-    return JSON.parse(state.text).posts;
-  } catch(err) {
-    console.log(err);
-    return;
-  }
-}
-
-ReactDOM.render(
-  <Store posts={tryReadPosts()}>
-    <App/>
-  </Store>,
-  document.getElementById('container'));
+ReactDOM.render(<App/>, document.getElementById('container'));
